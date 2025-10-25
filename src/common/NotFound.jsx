5fb71fd277ac00357eda5fba3e19fae29b40d517
Aaditya Vishnoi <vishnoiaaditya29@gmail.com>
2025-10-25 10:55:17 +0000

    commit

--- src/common/NotFound.jsx
+++ src/common/NotFound.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
-      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold text-gray-700 mb-2">Page Not Found</h2>
-      <p className="text-gray-500 mb-6">
+      <h1 className="text-6xl font-bold text-gray-800 mb-4 font-outfit">404</h1>
+      <h2 className="text-2xl font-semibold text-gray-700 mb-2 font-outfit">Page Not Found</h2>
+      <p className="text-gray-500 mb-6 font-outfit">
         The page you’re looking for doesn’t exist or has been moved.
       </p>
       <Link
         to="/"
-        className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all"
+        className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all font-outfit"
       >
         Go Back Home
       </Link>
