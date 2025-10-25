cf06914b4c075f7049bbc5e8aef47cc7e9a6e9c3
Aaditya Vishnoi <vishnoiaaditya29@gmail.com>
2025-10-25 10:56:44 +0000

    commit

--- src/common/NotFound.jsx
+++ src/common/NotFound.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import logo from "../assets/logo/logo.png";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+      <img src={logo} className="h-8" alt="Brainlink Software" />
       <h1 className="text-6xl font-bold text-gray-800 mb-4 font-outfit">404</h1>
       <h2 className="text-2xl font-semibold text-gray-700 mb-2 font-outfit">Page Not Found</h2>
       <p className="text-gray-500 mb-6 font-outfit">
