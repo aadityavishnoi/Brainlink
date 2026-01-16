3378b0833808eec61580b3e5f65e93be01415c90
aadityavishnoi <vishnoiaaditya29@gmail.com>
2026-01-16 21:53:42 +0530

    commit

---
+++ src/pages/Team.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import "../css/index.css";
+
+export default function Team() {
+    return(
+        <>
+        <Header />
+        
+        </>
+    );
+}
